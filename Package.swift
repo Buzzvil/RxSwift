c229163b2680d29dca6718535b65416cf583a7a8
Gongcu <knmy0101@naver.com>
2024-04-04 11:43:36 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -2,74 +2,19 @@
 
 import PackageDescription
 
-let buildTests = false
-
-extension Product {
-  static func allTests() -> [Product] {
-    if buildTests {
-      return [.executable(name: "AllTestz", targets: ["AllTestz"])]
-    } else {
-      return []
-    }
-  }
-}
-
-extension Target {
-  static func rxCocoa() -> [Target] {
-    #if os(Linux)
-      return [.target(name: "RxCocoa", dependencies: ["RxSwift", "RxRelay"])]
-    #else
-      return [.target(name: "RxCocoa", dependencies: ["RxSwift", "RxRelay", "RxCocoaRuntime"])]
-    #endif
-  }
-
-  static func rxCocoaRuntime() -> [Target] {
-    #if os(Linux)
-      return []
-    #else
-      return [.target(name: "RxCocoaRuntime", dependencies: ["RxSwift"])]
-    #endif
-  }
-
-  static func allTests() -> [Target] {
-    if buildTests {
-      return [.target(name: "AllTestz", dependencies: ["RxSwift", "RxCocoa", "RxBlocking", "RxTest"])]
-    } else {
-      return []
-    }
-  }
-}
-
 let package = Package(
-  name: "RxSwift",
-  platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v3), .tvOS(.v9)],
+  name: "BuzzRxSwift",
+  platforms: [.iOS(.v12)],
   products: ([
     [
-      .library(name: "RxSwift", targets: ["RxSwift"]),
-      .library(name: "RxCocoa", targets: ["RxCocoa"]),
-      .library(name: "RxRelay", targets: ["RxRelay"]),
-      .library(name: "RxBlocking", targets: ["RxBlocking"]),
-      .library(name: "RxTest", targets: ["RxTest"]),
-      .library(name: "RxSwift-Dynamic", type: .dynamic, targets: ["RxSwift"]),
-      .library(name: "RxCocoa-Dynamic", type: .dynamic, targets: ["RxCocoa"]),
-      .library(name: "RxRelay-Dynamic", type: .dynamic, targets: ["RxRelay"]),
-      .library(name: "RxBlocking-Dynamic", type: .dynamic, targets: ["RxBlocking"]),
-      .library(name: "RxTest-Dynamic", type: .dynamic, targets: ["RxTest"]),
+      .library(name: "BuzzRxSwift", targets: ["BuzzRxSwift"]),
+      .library(name: "BuzzRxSwift-Dynamic", type: .dynamic, targets: ["BuzzRxSwift"]),
     ],
-    Product.allTests()
   ] as [[Product]]).flatMap { $0 },
   targets: ([
     [
-      .target(name: "RxSwift", dependencies: []),
+      .target(name: "BuzzRxSwift", dependencies: []),
     ], 
-    Target.rxCocoa(),
-    Target.rxCocoaRuntime(),
-    [
-      .target(name: "RxRelay", dependencies: ["RxSwift"]),
-      .target(name: "RxBlocking", dependencies: ["RxSwift"]),
-      .target(name: "RxTest", dependencies: ["RxSwift"]),
-    ],
-    Target.allTests()
   ] as [[Target]]).flatMap { $0 },
   swiftLanguageVersions: [.v5]
 )
